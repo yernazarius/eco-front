9a67636a396830936bd661413d6cdbbe740d7ac9
yernazarius <ernzaryakupov04@gmail.com>
2024-07-31 20:45:52 +0500

    updated eco admin

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -6,7 +6,7 @@ import {
     removeFromStorage
 } from '@/services/auth-token.service'
 import { authService } from '@/services/auth.service'
-import config from '@/config/config'
+// import config from '@/config/config'
 
 const options: CreateAxiosDefaults = {
     baseURL: process.env.NEXT_PUBLIC_BASE_URL, // Use the BASE_URL from config
