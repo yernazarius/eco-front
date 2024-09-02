b3b8e19c5566836f21f9bbcdbe85fdfda9d28595
yernazarius <ernzaryakupov04@gmail.com>
2024-09-02 16:07:08 +0500

    new pages

--- src/app/(user)/contacts/layout.tsx
+++ src/app/(user)/contacts/layout.tsx
@@ -3,9 +3,18 @@ import type { PropsWithChildren } from 'react'
 
 
 export default function Layout({ children }: PropsWithChildren<unknown>) {
+    const menuItems = [
+        { href: '/services/verification-measurement', label: 'Поверка средств измерений' },
+        { href: '/services/faq-equipment', label: 'FAQ оборудование' },
+        { href: '/services/electronic-brochures', label: 'Электронные брошюры' },
+        { href: '/services/quality-certificates', label: 'Паспорта качества' },
+        { href: '/services/smart-solutions-labs', label: 'Умные решения для вашей лаборатории' },
+        { href: '/services/service-maintenance', label: 'Сервисное обслуживание ваших приборов' },
+        { href: '/services/online-industrial-solutions', label: 'Промышленные решения ON-LINE' },
+    ]
     return (
         <div className="container mx-auto px-20 mt-10 flex flex-col md:flex-row">
-            <VerticalSliderMenu />
+            <VerticalSliderMenu items={menuItems} />
             {children}
         </div>
     )
